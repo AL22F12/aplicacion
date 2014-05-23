202c97c65288c9e819d37fe22a59e7cb53bc0398
Victor Manuel Montes Hernandez <victor-1031@hotmail.com>
2014-05-23 09:18:23 -0500

    Tres
    esta es la tercera vez que lo hago!

---
+++ js/acciones.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+	$(document).ready(function(e){
+
+    $('#logo img').tap(function(e){	
+    $('#logo img').toggleClass('clicked');
+	$('#vista img').toggleClass('clicked');
+});//click 
+
+    $('#vista img').tap(function(e){	
+	 $("#roar")[0].play();	
+	 $.mobile.navigate( "#principal" );
+	});
+	
+	$('#B_Rex').tap(function (e) {
+ 	 $.mobile.navigate( "#Rex" );
+	});
+	
+	$('#mega_1').tap(function (e) {
+ 	 $.mobile.navigate( "#mega" );
+	 });
+	 $('#acerca').tap(function (e) {
+ 	 $.mobile.navigate( "#acerca_1" );
+	 });
+});//ready
